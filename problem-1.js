38b4f013aa3adeda4d1716ae852fbc4c4d7f5a94
amdadislam01 <amdadislam733@gmail.com>
2025-08-10 12:23:35 +0600

    Problem-1 Complate

---
+++ problem-1.js
@@ -0,0 +1,18 @@
+
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+console.log(celsiusToFahrenheit(0));   
+console.log(celsiusToFahrenheit(25));  
+console.log(celsiusToFahrenheit(100)); 
+
+
+/**
+ * Output :
+ * 1. 32
+ * 2. 77
+ * 3. 212
+ */
