a97f955559b0aaeb675455bb64f15659fbd6f76f
amdadislam01 <amdadislam733@gmail.com>
2025-08-10 12:41:45 +0600

    problem-6 complate

---
+++ problem-6.js
@@ -0,0 +1,14 @@
+/**
+ * Take four parameters. Multiply the four numbers and then return the result
+ */
+
+function multiplyFourNumbers(a, b, c, d) {
+  let result = a * b * c * d;
+  return result;
+}
+
+console.log(multiplyFourNumbers(5, 4, 2, 6)); 
+
+/**
+ * Output : 240
+ */
